d0675ffb7467deaaa654a7df874254e1a055587e
ika-si <ikachan.0630@gmail.com>
2021-02-27 21:46:59 +0900

    login機能を修正

---
+++ public/index.js
@@ -0,0 +1,26 @@
+function login_chack(){
+  firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+          document.getElementById("login-status").value="login"
+          window.location.assign('./index2.html');
+      }else{
+          document.getElementById("login-status").value="not logged"
+      }
+  });            
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+// 1st check
+// login_chack();
+
+// wite
+document.getElementById("login-btn").onclick = function() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+      login_chack();
+  }).catch(function(error) {
+      login_chack();
+  });
+};
+
+});
