1154989283f3d46c9c5f6e2dddd831eb75c0df48
SakuLin <g18911ws@gm.tsuda.ac.jp>
2021-03-09 00:20:28 +0900

    mainの変更を加えた

--- public/js/classlist.js
+++ public/js/classlist.js
@@ -65,6 +65,7 @@ function displayClass() {
 
             let newCB = document.createElement('div');
             newCB.setAttribute('class', 'form-check form-switch');
+            newCB.setAttribute('id', 'checkBtn');
 
                 let newCBinput = document.createElement('input');
                 newCBinput.setAttribute('class', 'form-check-input');
@@ -82,16 +83,19 @@ function displayClass() {
 
             let newDetail = document.createElement('div');
             newDetail.setAttribute('class', 'form-check form-switch');
+            newDetail.setAttribute('id', 'subjectID');
             newDetail.innerText = doc.data()['id'];
             newRow1.appendChild(newDetail);
 
             newDetail = document.createElement('div');
             newDetail.setAttribute('class', 'form-check form-switch');
+            newDetail.setAttribute('id', 'subjectName');
             newDetail.innerText = doc.data()['name'];
             newRow1.appendChild(newDetail);
 
             newDetail = document.createElement('div');
             newDetail.setAttribute('class', 'form-check form-switch');
+            newDetail.setAttribute('id', 'professor');
             newDetail.innerText = doc.data()['teacher'];
             newRow1.appendChild(newDetail);
 
