6028854bf5582e87df26ceda196f543793ecf84f
ika-si <ikachan.0630@gmail.com>
2021-03-09 17:46:32 +0900

    コメント追加　学年表記の処理

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,5 @@
 var db = firebase.firestore();
-let accountDoc, myEmail, myUndergraduate, myDepartment, myGrade, myTwitter, myInstagram;
+let accountDoc, myEmail, myUndergraduate, myDepartment, myGrade, myDetails, myTwitter, myInstagram;
 
 function showProfile(){
   db.collection("account").get().then((querySnapshot) => {
@@ -13,13 +13,20 @@ function showProfile(){
               myUndergraduate = doc.data()['undergraduate'];
               myDepartment = doc.data()['department'];
               myGrade = doc.data()['grade'];
+              let mySubGrade;
+              if (myGrade == -1) {
+                mySubGrade = "未入力"
+              } else {
+                mySubGrade = myGrade;
+              }
+              myDetails = doc.data()['details'];
               myTwitter = doc.data()['twitter'];
               myInstagram = doc.data()['instagram'];
-              console.log(userName + " "+ myEmail);
               $('#MyName').append('<h4>名前: ' + userName + '</h4>');
               $('#MyUndergraduate').append('<h4>学部: ' + myUndergraduate + '</h4>');
               $('#MyDepartment').append('<h4>学科: ' + myDepartment + '</h4>');
-              $('#MyGrade').append('<h4>学年: ' + myGrade + '</h4>');
+              $('#MyGrade').append('<h4>学年: ' + mySubGrade + '</h4>');
+              $('#MyDetails').append('<h4>コメント: ' + myDetails + '</h4>');
               $('#MyTwitter').append('<h4>' + myTwitter + '</h4>');
               $('#MyInstagram').append('<h4>' + myInstagram + '</h4>');
           }
@@ -38,31 +45,30 @@ function showNewProfile(){
   $('#MyUndergraduate').innerHTML = myUndergraduate;
   $('#MyDepartment').innerHTML = myDepartment;
   $('#MyGrade').innerHTML = myGrade;
+  $('#MyDtails').innerHTML = myDetails;
   $('#MyTwitter').innerHTML = myTwitter;
   $('#MyInstagram').innerHTML = myInstagram;
   document.location.href = '../account.html?name=' + encodeURIComponent(uid);
 }
 
 function changeProfile(){
-  //
+  
   let inputUndergraduate = $("#undergraduate").val();
-  // if (myUndergraduate == "undefined") myUndergraduate = "未入力";
   if (inputUndergraduate == "") inputUndergraduate = "未入力";
 
   let inputDepartment = $("#department").val();
-  // if (myDepartment == "undefined") myDepartment = "未入力";
   if (inputDepartment == "") inputDepartment = "未入力";
 
   let inputGrade = $("#grade").val();
-  // if (myGrade == "undefined") myGrade = "未入力";
   if (inputGrade == "") inputGrade = -1;
 
+  let inputDetails = $("#details").val();
+  if (inputDetails == "") inputDetails = "未入力";
+
   let inputTwitter = $("#twitter").val();
-  // if (myTwitter == "undefined") myTwitter = "未入力";
   if (inputTwitter == "") inputTwitter = "未入力";
 
   let inputInstagram = $("#instagram").val();
-  // if (myInstagram == "undefined") myInstagram = "未入力";
   if (inputInstagram == "") inputInstagram = "未入力";
 
   // Add a new document in collection "cities"
@@ -74,7 +80,8 @@ function changeProfile(){
       department: inputDepartment,
       grade: Number(inputGrade),
       twitter: inputTwitter,
-      instagram: inputInstagram
+      instagram: inputInstagram,
+      details: inputDetails
   })
   .then(() => {
       console.log("Document successfully written!");
